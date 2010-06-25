62f86dcd0bfe5ec53fe16edbb056cceb00e87c59
zaker <sondre.naustdal@gmail.com>
2010-06-25 11:20:53 +0200

    added structure and public functions

---
+++ point/point.go
@@ -0,0 +1,34 @@
+package point
+
+import(
+	"big"
+)
+
+
+type Point struct{
+	X *big.Int
+
+	Y *big.Int
+
+}
+
+func Add(J,K Point) Point {
+	var L Point
+
+
+	return L
+}
+
+func Nega(J Point) Point{
+	var L Point
+
+	return L
+}
+
+func Multi(k big.Int, P Point) Point{
+	var L Point
+
+
+	return L
+}
+
